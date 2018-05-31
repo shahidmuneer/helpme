8d8926d027cf86c758171fc1409438c8d2532143
shahidmuneer <m.shahidmuneer88@gmail.com>
2018-05-31 22:51:21 +0500

    added project to github'

---
+++ app/filterMenu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use JeroenNoten\LaravelAdminLte\Menu\Builder;
+use JeroenNoten\LaravelAdminLte\Menu\Filters\FilterInterface;
+use Laratrust;
+
+class filterMenu  extends Model implements FilterInterface
+{
+    public function transform($item, Builder $builder)
+    {
+        if (isset($item['permission']) && ! Laratrust::can($item['permission'])) {
+            return false;
+        }
+
+        return $item;
+    }
+}
